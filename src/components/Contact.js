7bb85408c2fdb2a95218e2538f26046060678f19
Daniel Rosenberg <rosenbergd36@gmail.com>
2020-02-19 22:12:45 -0800

    add container and card-panel to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,10 +4,10 @@ class Contact extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			fullName: 'Your name here',
-			email: 'example@gmail.com',
-			phone: '123-456-7890',
-			age: 0 
+			fullName: '',
+			email: '',
+			phone: '',
+			age: ''
 		};
 
 		this.handleChange = this.handleChange.bind(this);
@@ -34,45 +34,58 @@ class Contact extends Component {
 
 	render(){
 		return(
-			<div className='contactForm'>
+			<div className='contact'>
 				<h4 className='indigo lighten-2'>Get in Touch with Marilyn</h4>
-				<br></br>
-				<p>Want to start the conversation? Fill in your contact information below.</p>
-				<form onSubmit={this.handleSubmit}>
-					<label>
-						Enter your full name:
-						<input
-							name='fullName'
-							type='text'
-							value={this.state.fullName}
-							onChange={this.handleChange} />
-					</label>
-					<label>
-						Enter your email address:
-						<input
-							name='email'
-							type='text'
-							value={this.state.email}
-							onChange={this.handleChange} />
-					</label>
-					<label>
-						Enter your phone number:
-						<input
-							name='phone'
-							type='text'
-							value={this.state.phone}
-							onChange={this.handleChange} />
-					</label>
-					<label>
-						Enter your age: 
-						<input
-							name='age'
-							type='number'
-							value={this.state.age}
-							onChange={this.handleChange} />
-					</label>
-					<input type='submit' value='Submit' />
-				</form>
+					<div className='container'>
+						<div className='card-panel col s6'>
+							<p>Want to start the conversation? Fill in your contact information below.</p>
+							<form onSubmit={this.handleSubmit}>
+								<div className='input field col s6'>
+									<label>
+										Enter your full name:
+										<input
+											placeholder='Your Name'
+											name='fullName'
+											type='text'
+											value={this.state.fullName}
+											onChange={this.handleChange} />
+									</label>
+								</div>
+								<div className='input field col s6'>
+									<label>
+										Enter your email address:
+										<input
+											name='email'
+											type='text'
+											value={this.state.email}
+											onChange={this.handleChange} />
+									</label>
+								</div>
+								<div className='input field col s6'>
+									<label>
+										Enter your phone number:
+										<input
+											name='phone'
+											type='text'
+											value={this.state.phone}
+											onChange={this.handleChange} />
+									</label>
+								</div>
+								<div className='input field col s6'>
+									<label>
+										Enter your age:
+										<input
+											name='age'
+											type='number'
+											value={this.state.age}
+											onChange={this.handleChange} />
+									</label>
+								</div>
+							</form>
+					<br></br>
+					<button className="btn waves-effect waves-light indigo-lighten-2" type="submit">Send</button>
+					</div>
+				</div>
 			</div>
 		);
 	}
